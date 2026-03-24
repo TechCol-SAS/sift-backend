17680d14bc945e66d5c299fed97048afb36512d6
KevinDanielDev <k4vind@gmail.com>
2026-03-24 16:54:26 -0500

    Creación de campos de DTOs + creación de la entidad de usuarios

--- src/user/dto/signup.dto.ts
+++ src/user/dto/signup.dto.ts
@@ -1,1 +1,39 @@
-export class SignUpDto {}
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class SignUpDto {
+  @IsString()
+  @IsOptional()
+  id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  @MaxLength(255)
+  email: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  isActive: boolean;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(16)
+  password: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export default class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'first_name', nullable: false })
+  firstName: string;
+
+  @Column({ name: 'last_name', nullable: false })
+  lastName: string;
+
+  @Column({ nullable: false, unique: true })
+  email: string;
+
+  @Column({ name: 'is_active', nullable: false, default: true })
+  isActive: boolean;
+
+  @Column({ nullable: false })
+  password: string;
+}
